8b6bebcacbc1284e28ecbdfbeac65f5f41a30741
JyotiKhabra <44710571+JyotiKhabra@users.noreply.github.com>
2020-05-12 14:46:57 -0700

    finish setting up interviewerlist and interviewerlistitme

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,16 +1,25 @@
 import React from "react";
 import "components/InterviewerListItem.scss";
+const classNames = require('classnames');
 
 
 export default function InterviewerListItem(props) {
+  const interviewerListItem = classNames({
+    'interviewers__item': true, 
+    'interviewers__item-image': props.avatar,
+    'interviewers__item--selected': props.selected
+  });
   return (
-    <li className="interviewers__item">
+    <li 
+      className={interviewerListItem} 
+      onClick={() => props.setInterviewer(props.name)}
+    >
     <img
-    className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
+      className="interviewers__item-image"
+      src={props.avatar}
+      alt={props.name}
     />
-    Sylvia Palmer
+    {props.selected && props.name}
    </li>
   );
 }
