cb569bcca319f4d32d680eb24403bd496879e3dd
JyotiKhabra <44710571+JyotiKhabra@users.noreply.github.com>
2020-05-13 13:45:23 -0700

    Displaying apopintments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, {Fragment} from "react";
 import "components/Appointment/styles.scss";
 import Show from "components/Appointment/Show";
 import Header from "components/Appointment/Header";
@@ -8,18 +8,14 @@ import Empty from "components/Appointment/Empty";
 
 
 export default function Appointment(props) {
-  props.interview() ? Show : Empty
+
   return (
-    <Fragment>
-    <Header
-    time={props.time}
-    />
-    <Show
-    />
-    <Empty/>
-    <Appointment id="last" time="1pm" />
-  
-   </Fragment>
-    //<article className="appointment"></article>
-  );
-}
+    <article className="appointment">
+      <Header
+        time={props.time}
+      />
+     {props.interview ?(  
+     <Show   student={props.interview.student}
+     name={props.interview.interviewer.name}/>) : (<Empty />)}
+    </article>
+  )}
