29f0160d4d426b62c7cffcea9d2dffb2036faeed
JyotiKhabra <44710571+JyotiKhabra@users.noreply.github.com>
2020-05-12 22:53:59 -0700

    Displaying Appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "components/Appointment/styles.scss";
+import Show from "components/Appointment/Show";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+
 //const classNames = require('classnames');
 
 
 export default function Appointment(props) {
+  props.interview() ? Show : Empty
   return (
-    <article className="appointment"></article>
+    <Fragment>
+    <Header
+    time={props.time}
+    />
+    <Show
+    />
+    <Empty/>
+    <Appointment id="last" time="1pm" />
+  
+   </Fragment>
+    //<article className="appointment"></article>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -187,4 +187,20 @@ storiesOf("Button", module)
              onCancel={action('onCancel')}
              onSave={action('onSave')}
              />
-           ));
+           ))
+           .add("Appointment Empty", () => (
+            <Fragment>
+              <Appointment id={1} time="12pm" />
+              <Appointment id="last" time="1pm" />
+            </Fragment>
+          ))
+          .add("Appointment Booked", () => (
+            <Fragment>
+              <Appointment
+                id={1}
+                time="12pm"
+                interview={{ student: "Lydia Miller-Jones", interviewer }}
+              />
+              <Appointment id="last" time="1pm" />
+            </Fragment>
+          ));
